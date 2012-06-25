b97dbbb794dc8d7f3a28de113be8da46c058ea98
Felix C. Stegerman <flx@obfusk.net>
2012-06-25 22:37:15 +0200

    ~ mgit.bash (from sodium)
    Signed-off-by: Felix C. Stegerman <flx@obfusk.net>

--- mgit.bash
+++ mgit.bash
@@ -1,1 +1,19 @@
-# ...
+#!/bin/bash
+
+function die () { echo "error: $1" >&2; exit 1; }
+
+if [ -n "$MGIT_DIRS" ]; then
+  ds=( $MGIT_DIRS )
+else
+  ds=( $( find . -name '.git' | sed 's!/\.git$!!' | sort ) )
+fi
+
+for d in "${ds[@]}"; do
+  echo "==> $d <=="
+  pushd "$d" > /dev/null || die "failed to pushd $d"
+    git "$@"
+  popd > /dev/null
+  echo
+done
+
+# --
