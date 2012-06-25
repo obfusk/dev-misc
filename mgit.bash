adbf0096e491326a52fb5a37ffaba27876a2c387
Felix C. Stegerman <flx@obfusk.net>
2012-06-25 22:55:12 +0200

    ~ mgit (updated from sodium)
    Signed-off-by: Felix C. Stegerman <flx@obfusk.net>

--- mgit.bash
+++ mgit.bash
@@ -4,6 +4,12 @@ function die () { echo "error: $1" >&2; exit 1; }
 
 if [ -n "$MGIT_DIRS" ]; then
   ds=( $MGIT_DIRS )
+elif [ "$1" == STDIN ]; then
+  ds=() ; shift ; while read -r; do ds+=( "$REPLY" ); done
+elif [ "$1" == DIR ]; then
+  ds=( "$2" ) ; shift 2
+elif [ "$1" == DIRS ]; then
+  ds=( "$2"/* ) ; shift 2
 else
   ds=( $( find . -name '.git' | sed 's!/\.git$!!' | sort ) )
 fi
